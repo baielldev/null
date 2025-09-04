5145c97f1552343b77c5438317582a4c38bef6b3
baielldev <bajel8844@gmail.com>
2025-09-04 18:30:17 +0600

    .

--- src/components/heroVideo/HeroVideo.jsx
+++ src/components/heroVideo/HeroVideo.jsx
@@ -3,7 +3,7 @@ export default function HeroVideo() {
   return (
     <div className="main">
       <iframe
-        src="https://drive.google.com/file/d/1JZmFacPKy8g62jVWiLNl6oirfbYS3cak/preview"
+        src="https://drive.google.com/file/d/1gCOXss5A-CIn6KoLMmj_zwQjJ59WMrf3/preview"
         width="100%"
         height="400"
         allow="autoplay"
